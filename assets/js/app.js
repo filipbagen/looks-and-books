1c1a09f81bb108b62356897309f83d49568b1f42
Filip Malm-Bägén <Filip.bagen@gmail.com>
2022-04-16 22:37:53 +0200

    Added slideshow

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
 const init = () => {
     highlightDayOfTheWeek()
+    window.setTimeout(function () { autoScrollSlideshow() }, 4000)
 }
 
 function getToday () {
@@ -9,4 +10,39 @@ function getToday () {
 function highlightDayOfTheWeek() {
     const target = /** @type {HTMLElement} */ (document.querySelector('#OpeningHours :nth-child(' + getToday() + ')'))
     target.classList.add('today')
+}
+
+function slideshow (goTo) {
+  const container = document.querySelector('#imageContainer')
+  const oldDot = document.querySelector('#page3 #dots .active')
+  const childs = document.querySelectorAll('#dots a')
+  if (goTo === undefined && childs[0]) {
+    const current = document.querySelector('#dots .active')
+    for (let i = 0; i < childs.length; i++) {
+      if (childs[i] === current) {
+        goTo = i + 2
+        if (goTo > childs.length) {
+          goTo = 1
+        }
+      }
+    }
   }
+  const dot = document.querySelector('#slideshow' + goTo)
+  if (oldDot) {
+    oldDot.classList.remove('active')
+  }
+  if (dot) {
+    dot.classList.add('active')
+  }
+  if (container) {
+    container.style.transform = 'translateX(' + -100 / childs.length * (goTo - 1) + '%)'
+  }
+}
+
+function autoScrollSlideshow () {
+  const loop = function () {
+    slideshow()
+    window.setTimeout(function () { loop() }, 4000)
+  }
+  loop()
+}
